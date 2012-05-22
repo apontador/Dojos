1cb9d20490b764ec7de72877c656c826d93e46dc
Rodrigo Prestes <rodrigo.prestes@apontador.com>
2012-05-22 17:52:29 -0300

    implementacao para 1 bomba em todos os lados

---
+++ campo-minado/CampoMinado.php
@@ -0,0 +1,65 @@
+<?php
+
+define("BOMBA", "*");
+
+class CampoMinado
+{
+	private $mapaEntrada;
+
+	public function __construct($entrada)
+	{
+		$this->mapaEntrada = $entrada;
+	}
+
+
+	public function getMapa() 
+	{
+		$mapaResultado = $this->mapaEntrada;
+		for($linha=0; $linha < count($this->mapaEntrada); $linha++) {
+			for($coluna=0; $coluna < count($this->mapaEntrada[$linha]); $coluna++)
+			{
+				if ($this->mapaEntrada[$linha][$coluna] == BOMBA) {
+					$this->populaAdjacentesDaBomba(&$mapaResultado, $linha, $coluna);
+				}
+			}
+		}
+
+		return $mapaResultado;
+	}
+
+	public function populaAdjacentesDaBomba($mapaResultado, $linha, $coluna) {
+		
+		if (isset($mapaResultado[$linha][$coluna + 1])) {
+			$mapaResultado[$linha][$coluna + 1] = "1";
+		}
+
+		if (isset($mapaResultado[$linha][$coluna - 1])) {
+			$mapaResultado[$linha][$coluna - 1] = "1";
+		}
+		
+		if (isset($mapaResultado[$linha + 1][$coluna])) {
+			$mapaResultado[$linha + 1][$coluna]   = "1";
+		}
+		
+		if (isset($mapaResultado[$linha + 1][$coluna + 1])) {
+			$mapaResultado[$linha + 1][$coluna + 1] = "1";
+		}
+
+		if (isset($mapaResultado[$linha +1][$coluna - 1])) {
+			$mapaResultado[$linha + 1][$coluna - 1] = "1";
+		}
+
+		if (isset($mapaResultado[$linha - 1][$coluna])) {
+			$mapaResultado[$linha - 1][$coluna]   = "1";
+		}
+		
+		if (isset($mapaResultado[$linha - 1][$coluna + 1])) {
+			$mapaResultado[$linha - 1][$coluna + 1] = "1";
+		}
+
+		if (isset($mapaResultado[$linha - 1][$coluna - 1])) {
+			$mapaResultado[$linha - 1][$coluna - 1] = "1";
+		}
+
+	}
+}
---
+++ campo-minado/CampoMinadoTest.php
@@ -0,0 +1,134 @@
+<?php
+
+require_once('CampoMinado.php');
+
+class CampoMinadoTest extends PHPUnit_Framework_TestCase
+{
+	// public function testUmaBomba()
+	// {
+	// 	$entrada = array(
+	// 			array("*", "0", "0","0"),
+	// 			array("0", "0", "0","0"),
+	// 			array("0", "0", "0","0"),
+	// 			array("0", "0", "0","0"),
+	// 		);
+	// 	$campoMinado = new CampoMinado($entrada);
+
+	// 	$esperado = array(
+	// 			array("*", "1", "0", "0"),
+	// 			array("1", "1", "0", "0"),
+	// 			array("0", "0", "0", "0"),
+	// 			array("0", "0", "0", "0"),
+	// 		);
+
+	// 	$this->assertSame($esperado, $campoMinado->getMapa());
+
+	// }
+
+	public function test1x1ComBomba()
+	{
+		$entrada = array(
+				array("*"),
+				
+			);
+		$campoMinado = new CampoMinado($entrada);
+
+		$esperado = array(
+				array("*"),
+				
+			);
+
+		$this->assertSame($esperado, $campoMinado->getMapa());
+	}
+
+	public function test1x1SemBomba()
+	{
+		$entrada = array(
+				array("0"),
+			);
+		$campoMinado = new CampoMinado($entrada);
+
+		$esperado = array(
+				array("0"),
+			);
+
+		$this->assertSame($esperado, $campoMinado->getMapa());
+	}
+
+	public function test2x2SemBomba()
+	{
+		$entrada = array(
+				array("0", "0"),
+				array("0", "0")
+			);
+		$campoMinado = new CampoMinado($entrada);
+
+		$esperado = array(
+				array("0", "0"),
+				array("0", "0"),
+			);
+		$this->assertSame($esperado, $campoMinado->getMapa());
+	}
+
+	public function test2x2Com1Bomba()
+	{
+		$entrada = array(
+				array("*", "0"),
+				array("0", "0")
+			);
+		$campoMinado = new CampoMinado($entrada);
+
+		$esperado = array(
+				array("*", "1"),
+				array("1", "1"),
+			);
+		$this->assertSame($esperado, $campoMinado->getMapa());
+	}
+
+	public function test2x2Com1BombaADireitaSuperior()
+	{
+		$entrada = array(
+				array("0", "*"),
+				array("0", "0")
+			);
+		$campoMinado = new CampoMinado($entrada);
+
+		$esperado = array(
+				array("1", "*"),
+				array("1", "1"),
+			);
+		$this->assertSame($esperado, $campoMinado->getMapa());
+	}
+
+		public function test2x2Com1BombaADireitaInferior()
+	{
+		$entrada = array(
+				array("0", "0"),
+				array("0", "*")
+			);
+		$campoMinado = new CampoMinado($entrada);
+
+		$esperado = array(
+				array("1", "1"),
+				array("1", "*"),
+			);
+		$this->assertSame($esperado, $campoMinado->getMapa());
+	}
+
+		public function test2x2Com1BombaAEsquerdaInferior()
+	{
+		$entrada = array(
+				array("0", "0"),
+				array("*", "0")
+			);
+		$campoMinado = new CampoMinado($entrada);
+
+		$esperado = array(
+				array("1", "1"),
+				array("*", "1"),
+			);
+		$this->assertSame($esperado, $campoMinado->getMapa());
+	}
+
+
+}
